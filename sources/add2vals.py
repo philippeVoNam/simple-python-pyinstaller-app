29336fa30acdb521c82df3727586f2669c0579e4
Philippe Vo <philippe.vo.nam@gmail.com>
2024-01-29 14:09:11 -0500

    made changes to src code

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -11,6 +11,7 @@ argnumbers = len(sys.argv) - 1
 if argnumbers == 2 :
     print("")
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
+    print("Thank you for your input")
     print("")
     sys.exit(0)
 
